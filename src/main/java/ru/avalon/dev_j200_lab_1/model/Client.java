eaa8b887e6fffc3fc97f1c2ba548da5a18b6571b
Crevan <a.siverskii@gmail.com>
2022-11-21 21:50:00 +0100

    [update] Client

--- src/main/java/ru/avalon/dev_j200_lab_1/model/Client.java
+++ src/main/java/ru/avalon/dev_j200_lab_1/model/Client.java
@@ -1,20 +1,52 @@
 package ru.avalon.dev_j200_lab_1.model;
 
-public class Client {
-    private final String name;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-    public Client(String name) {
+public class Client {
+    private final int id;
+    private final String name;
+    private final String type;
+    private final Date added;
+    private final List<Address> addresses;
+
+    public Client(int id, String name, String type, Date added) {
+        this.id = id;
         this.name = name;
+        this.type = type;
+        this.added = added;
+        this.addresses = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public Date getAdded() {
+        return added;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
     @Override
     public String toString() {
         return "Client{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", added=" + added +
+                ", addresses=" + addresses +
                 '}';
     }
 }
