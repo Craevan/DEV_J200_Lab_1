1dc77464b2b2012db1fd45575ab90449becb61fa
Crevan <a.siverskii@gmail.com>
2022-12-04 20:50:25 +0100

    [update] add action delete processing

--- src/main/java/ru/avalon/dev_j200_lab_1/servlet/ClientServlet.java
+++ src/main/java/ru/avalon/dev_j200_lab_1/servlet/ClientServlet.java
@@ -1,9 +1,11 @@
 package ru.avalon.dev_j200_lab_1.servlet;
 
 import ru.avalon.dev_j200_lab_1.dao.InMemoryDao;
+import ru.avalon.dev_j200_lab_1.model.Client;
 
 import java.io.*;
-import javax.servlet.ServletException;
+import java.util.Objects;
+import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
@@ -20,8 +22,20 @@ public class ClientServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
+        String action = request.getParameter("action");
+        Client client;
+        if ("delete".equals(action)) {
+            int id = getId(request);
+            db.delete(id);
+            response.sendRedirect("clients");
+        } else {
+            request.setAttribute("clientToList", db.read());
+            request.getRequestDispatcher("clients.jsp").forward(request, response);
+        }
+    }
 
-        request.setAttribute("clientToList", db.read());
-        request.getRequestDispatcher("clients.jsp").forward(request, response);
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.parseInt(paramId);
     }
 }
